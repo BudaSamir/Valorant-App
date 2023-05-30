bcbf9aac947ca0f5dc6ea07da12e3b8379f12269
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-05-30 15:01:44 +0300

    Created AgentApi class then Imp it AND creaded  Agents dependency injection at AppConstants

--- lib/core/services/network_service.dart
+++ lib/core/services/network_service.dart
@@ -1,5 +1,13 @@
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 abstract class NetworkService {
-  Future<Response> get(String url);
+  Future<http.Response> get(String url);
+}
+
+class AgentAPI implements NetworkService {
+  @override
+  Future<http.Response> get(String url) async {
+    final response = await http.get(Uri.parse(url));
+    return response;
+  }
 }
--- lib/core/utils/constants/constant.dart
+++ lib/core/utils/constants/constant.dart
@@ -1,6 +1,10 @@
+import '../../services/network_service.dart';
+
 class AppConstants {
   static const String baseUrl = "https://valorant-api.com/v1";
   // Agents API URL
   static const String agentEndpoint = "/agents";
   static const String agentAPI = baseUrl + agentEndpoint;
+  // Agents dependency injection
+  static NetworkService agentApiProvider = AgentAPI();
 }
