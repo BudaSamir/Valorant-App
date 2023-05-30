76310a8d7251aecc2622daef74cec6e5595b0bcd
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-05-30 14:35:55 +0300

    added App Constants

---
+++ lib/core/utils/constants/constant.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const String baseUrl = "https://valorant-api.com/v1";
+  // Agents API URL
+  static const String agentEndpoint = "/agents";
+  static const String agentAPI = baseUrl + agentEndpoint;
+}
